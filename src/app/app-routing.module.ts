90b3fe062396129f56ff084ccc220bbf58efcc90
jaydiprakholiya <jaydiprakholiya1611@gmail.com>
2021-05-25 12:19:05 +0530

    nevi work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
-import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
-
-
 
 const routes: Routes = [
-  { path: 'employee', component: EmployeeComponent },
- 
-  {path:'detail/:id',component:EmployeeDetailComponent},  
-   //The colon (:) in the path indicates that :id is a placeholder for a specific hero id.
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'employee', component: EmployeeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
@@ -18,7 +19,8 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
