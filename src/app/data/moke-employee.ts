550a648fa8420c90e481aa74db9b8fce074996e7
jaydiprakholiya <jaydiprakholiya1611@gmail.com>
2021-05-26 14:07:12 +0530

    name add

--- src/app/data/moke-employee.ts
+++ src/app/data/moke-employee.ts
@@ -5,5 +5,9 @@ export const EMPLOYEE:employee[]=[
     {id:13,name:'jd',MobileNo:357987654},
     {id:14,name:'Aashish',MobileNo:147258369},
     {id:15,name:'jemin',MobileNo:159487263},
-    {id:16,name:'raj',MobileNo:7983241654}
+    {id:16,name:'raj',MobileNo:7983241654},
+    {id:17,name:'rajesh',MobileNo:9635238082},
+    {id:18,name:'gajju',MobileNo:9635238082},
+    {id:19,name:'paresh',MobileNo:9635238082},
+    {id:20,name:'ramji',MobileNo:9635238082},
 ];
